62a89270881b92bb33cf8d4e163935c1c9ea62cd
Maksim_Marchuk <makstop280409@gmail.com>
2026-06-07 11:50:43 +0300

    asdasdas

---
+++ add.js
@@ -0,0 +1,22 @@
+var print = "Hello World";
+
+console.log(print.split(" "));
+
+var fruits = ["apple", "orange", "lemon"]
+
+console.log(fruits.join(" - "))
+
+var fruit = "Gitler"
+
+console.log(fruits.includes(fruit));
+fruits.push("88")
+
+console.log(fruits)
+
+fruits.pop()
+
+console.log(fruits)
+
+fruits.unshift("Adolf")
+
+console.log(fruits)
